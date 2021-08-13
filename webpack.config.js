96fb419fc5acf180982fb84bfdf00f2093e24d4f
Mystic <215104920@qq.com>
2021-08-13 17:59:59 +0800

    feat(migrating): Prepared for js2ts migration

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    entry: './app.ts',
+    output: {
+        filename: './dist/bundle.js'
+    },
+
+    // Enable sourcemaps for debugging webpack output.
+    devtool: 'source-map',
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js']
+    },
+
+    module: {
+        loaders: [
+            // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader'
+            }
+        ],
+
+        preLoaders: [
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                test: /\.js$/,
+                loader: 'source-map-loader'
+            }
+        ]
+    }
+}
