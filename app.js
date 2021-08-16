e72171955238ffea03235137ba11d6db6769c9b4
Mystic <215104920@qq.com>
2021-08-16 15:21:35 +0800

    feat: introduce koa-body

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const { registerRouter } = require('./router/loader')
 const { listener } = require('./scheduler/scheduler')
 const { LOG } = require('./utils/log')
 
+// body parser
+const koaBody = require('koa-body')
+app.use(koaBody({
+    multipart: true,
+    // Patch request body to Node's ctx.req
+    patchNode: true
+}))
+
 const appServer = createServer(app.callback())
 listener(appServer)
 
--- router/user_router.js
+++ router/user_router.js
@@ -1,15 +1,17 @@
+const { LOG } = require('../utils/log')
 const Router = require('@koa/router')
 const router = new Router()
 
 router.prefix('/user')
 
 router.get('/hi', async (ctx) => {
+    LOG.info(`[${ctx.request.method}] ${ctx.request.url}`)
     ctx.type = 'html'
     ctx.body = '<h1>hello world! User.</h1>'
 })
 
 router.get('/', async (ctx) => {
-    console.log('hello world! Console.')
+    LOG.info(`[${ctx.request.method}] ${ctx.request.url}`)
     ctx.body = '<h1>Page existed.</h1>'
 })
 
